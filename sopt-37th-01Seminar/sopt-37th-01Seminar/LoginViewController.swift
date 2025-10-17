cdd4e25a938c632fb1f0d794a3c65dc4faae2ce2
JaeSunEo <wotjs020708@gmail.com>
2025-10-17 11:06:28 +0900

    refactor: #1 기본 로그인 UI 수정

--- sopt-37th-01Seminar/sopt-37th-01Seminar/LoginViewController.swift
+++ sopt-37th-01Seminar/sopt-37th-01Seminar/LoginViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 final class LoginViewController: UIViewController {
     
     private let titleLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: 69, y: 161, width: 236, height: 44))
+        let label = UILabel(frame: CGRect(x: 70, y: 163, width: 236, height: 44))
         label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
         label.textColor = .black
         label.textAlignment = .center
@@ -21,27 +21,35 @@ final class LoginViewController: UIViewController {
     
     private let idTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
-        textField.placeholder = "아이디를 입력해주세요"
+        textField.placeholder = "아이디"
         textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
         textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.layer.cornerRadius = 3
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        
         return textField
     }()
     
     private let passwordTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
-        textField.placeholder = "비밀번호를 입력해주세요"
+        textField.placeholder = "비밀번호"
         textField.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+        textField.isSecureTextEntry = trueadfasf
         textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        textField.layer.cornerRadius = 3
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
         return textField
     }()
     
     private lazy var loginButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
         button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.layer.cornerRadius = 6
         button.setTitle("로그인하기", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
-//        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
         button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
         return button
     }()
@@ -62,7 +70,6 @@ final class LoginViewController: UIViewController {
     private func presentToWelcomeVC() {
         let vc = WelcomeViewController()
         vc.modalPresentationStyle = .overFullScreen
-        vc.modalTransitionStyle = .flipHorizontal
         vc.name = idTextField.text
         self.present(vc, animated: true)
     }
