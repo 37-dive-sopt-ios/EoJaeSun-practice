2a966b25617ebda219646baf487c744f0be75bac
JaeSunEo <wotjs020708@gmail.com>
2025-11-01 15:17:05 +0900

    테이블 뷰 예제

--- sopt-37th-01Seminar/sopt-37th-03Seminar/ChatViewController.swift
+++ sopt-37th-01Seminar/sopt-37th-03Seminar/ChatViewController.swift
@@ -5,4 +5,68 @@
 //  Created by 어재선 on 11/1/25.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class ChatViewController: UIViewController {
+    
+    private let tableView = UITableView(frame: .zero, style: .plain) // 테이블 뷰 객체 초기화
+    
+    private var chatRooms: [ChatRoomModel] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+        setLayout()
+    }
+    
+    private func setUI() {  // 기본 속성 정의
+        view.backgroundColor = .white
+        title = "채팅"
+        tableView.separatorStyle = .singleLine
+    }
+    
+    private func setLayout() {           // 레이아웃 잡기~!
+        self.view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func setDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func register() {
+        tableView.register(ChatsTableViewCell.self, forCellReuseIdentifier: ChatsTableViewCell.identifier)
+    }
+}
+
+
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(chatRooms[indexPath.row].name) 채팅방 선택됨")
+    }
+    
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
+}
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatTableViewCell.identifier, for: indexPath) as? ChatTableViewCell else {
+                    return UITableViewCell()
+                }
+                
+                cell.configure(with: chatRooms[indexPath.row])
+                return cell
+    }
+    
+    
+}
