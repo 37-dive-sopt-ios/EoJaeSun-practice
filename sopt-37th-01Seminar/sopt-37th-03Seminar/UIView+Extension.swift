8905ef93ba8e98e2af717efe9e16472962bae257
JaeSunEo <wotjs020708@gmail.com>
2025-11-01 15:16:34 +0900

    addSubviews 확장으로 추가

--- sopt-37th-01Seminar/sopt-37th-03Seminar/UIView+Extension.swift
+++ sopt-37th-01Seminar/sopt-37th-03Seminar/UIView+Extension.swift
@@ -4,5 +4,13 @@
 //
 //  Created by 어재선 on 11/1/25.
 //
+    
+import UIKit
 
-import Foundation
+extension UIView {
+
+    /// UIView 여러 개 인자로 받아서 한 번에 addSubview 합니다.
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
