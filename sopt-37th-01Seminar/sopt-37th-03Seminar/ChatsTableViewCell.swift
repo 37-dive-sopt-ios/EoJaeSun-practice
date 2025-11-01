b9e4cec1568b5dd39e6c527a6521e6de01bc62a2
JaeSunEo <wotjs020708@gmail.com>
2025-11-01 15:15:49 +0900

    이미지 파일 변경 및 기본 파일 설정

---
+++ sopt-37th-01Seminar/sopt-37th-03Seminar/ChatsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatsTableViewCell.swift
+//  sopt-37th-01Seminar
+//
+//  Created by 어재선 on 11/1/25.
+//
+
+import UIKit
+
+class ChatsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ sopt-37th-01Seminar/sopt-37th-03Seminar/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  sopt-37th-01Seminar
+//
+//  Created by 어재선 on 11/1/25.
+//
+
+import Foundation
